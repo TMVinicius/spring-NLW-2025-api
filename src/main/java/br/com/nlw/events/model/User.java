7fef6d459620b5c6fdf71924e51edb4f92fe0b10
TMVinicius <viniciustavaresbr@gmail.com>
2025-02-22 09:34:44 -0300

    Updated class Subscription

--- src/main/java/br/com/nlw/events/model/User.java
+++ src/main/java/br/com/nlw/events/model/User.java
@@ -15,7 +15,7 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "Ouser_id")
+	@Column(name = "user_id")
 	private Integer id;
 	
 	@Column(name = "user_name", length = 255, nullable = false)
--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -3,8 +3,10 @@ package br.com.nlw.events.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicatorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -17,36 +19,43 @@ public class SubscriptionService {
 
 	@Autowired
 	private EventRepo evtRepo;
-	
+
 	@Autowired
 	private UserRepo userRepo;
-	
+
 	@Autowired
 	private SubscriptionRepo subRepo;
-	
-	public Subscription createNewSubscription(String eventName, User user) {
-		
+
+	public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId) {
+
 		Event evt = evtRepo.findByPrettyName(eventName);
 		if (evt == null) {
-			throw new EventNotFoundException("Evento "+eventName+" nao existe");
+			throw new EventNotFoundException("Evento " + eventName + " nao existe");
 		}
-		
+
 		User userRec = userRepo.findByEmail(user.getEmail());
 		if (userRec == null) {
 			userRec = userRepo.save(user);
 		}
 		
+		User indicator = userRepo.findById(userId).orElse(null);
+		if (indicator == null) {
+			throw new UserIndicatorNotFoundException("Usuario " + userId+ " indicador nao existe");
+		}
+				
+
 		Subscription subs = new Subscription();
 		subs.setEvent(evt);
 		subs.setSubscriber(userRec);
+		subs.setIndication(indicator);
 		
 		Subscription tmpSub = subRepo.findByEventAndSubscriber(evt, userRec);
 		if (tmpSub != null) {
-			throw new SubscriptionConflictException("Ja existe incricao para o usuario "+ userRec.getName());
+			throw new SubscriptionConflictException("Ja existe incricao para o usuario " + userRec.getName() + " no evento " + evt.getTitle());
 		}
-		
+
 		Subscription res = subRepo.save(subs);
-		return res;
+		return new SubscriptionResponse(res.getSubscriptionNumber(), "http://codecraft.com/subscription/"+res.getEvent().getPrettyName()+"/"+res.getSubscriber().getId());
 	}
-	
+
 }
